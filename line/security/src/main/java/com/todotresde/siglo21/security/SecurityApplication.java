7a88e885131fd74f00c7de3d7a1c63d324f31968
Leonardo Garay <todotresde@gmail.com>
2017-02-24 07:23:52 -0300

    Freeze code

--- line/security/src/main/java/com/todotresde/siglo21/security/SecurityApplication.java
+++ line/security/src/main/java/com/todotresde/siglo21/security/SecurityApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages="com.todotresde.siglo21.security")
--- line/security/src/main/java/com/todotresde/siglo21/security/configuration/SecurityConfiguration.java
+++ line/security/src/main/java/com/todotresde/siglo21/security/configuration/SecurityConfiguration.java
@@ -32,28 +32,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.OPTIONS, "/**");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-                .and()
-                .authorizeRequests()
-                .antMatchers("/**").authenticated()
-                .and()
-                .httpBasic();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("reader")
-                .password("reader")
-                .authorities("FOO_READ")
-                .and()
-                .withUser("writer")
-                .password("writer")
-                .authorities("FOO_READ", "FOO_WRITE");
-    }
 }
