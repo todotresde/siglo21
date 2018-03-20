aee6b89d809bebde08acb8cf8d8fef4b33d62de8
Leonardo Garay <todotresde@gmail.com>
2018-03-20 07:22:36 -0300

    Partially allow to edit restore a Manufacturing Order

--- tesis/sfi2/src/main/webapp/app/entities/manufacturing-order/manufacturing-order-popup.service.ts
+++ tesis/sfi2/src/main/webapp/app/entities/manufacturing-order/manufacturing-order-popup.service.ts
@@ -5,6 +5,12 @@ import { HttpResponse } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
 import { ManufacturingOrder } from './manufacturing-order.model';
 import { ManufacturingOrderService } from './manufacturing-order.service';
+import { ManufacturingOrderDTO } from '../manufacturing-order-dto/manufacturing-order-dto.model';
+import { STAttributeValue } from '../st-attribute-value/st-attribute-value.model';
+import { Supply } from '../supply/supply.model';
+import { SupplyType } from '../supply-type/supply-type.model';
+import { STAttribute } from '../st-attribute/st-attribute.model';
+import { Product } from '../product/product.model';
 
 @Injectable()
 export class ManufacturingOrderPopupService {
@@ -30,10 +36,8 @@ export class ManufacturingOrderPopupService {
             if (id) {
                 this.manufacturingOrderService.findFull(id)
                     .subscribe((manufacturingOrderResponse: HttpResponse<ManufacturingOrder>) => {
-                        const manufacturingOrder: ManufacturingOrder = manufacturingOrderResponse.body;
-                        manufacturingOrder.orderDate = this.datePipe
-                            .transform(manufacturingOrder.orderDate, 'yyyy-MM-ddTHH:mm:ss');
-                        this.ngbModalRef = this.manufacturingOrderModalRef(component, manufacturingOrder);
+                        const manufacturingOrderDTO: ManufacturingOrderDTO = manufacturingOrderResponse.body;
+                        this.ngbModalRef = this.manufacturingOrderModalRef(component, manufacturingOrderDTO);
                         resolve(this.ngbModalRef);
                     });
             } else {
@@ -46,9 +50,55 @@ export class ManufacturingOrderPopupService {
         });
     }
 
-    manufacturingOrderModalRef(component: Component, manufacturingOrder: ManufacturingOrder): NgbModalRef {
+    manufacturingOrderModalRef(component: Component, manufacturingOrderDTO: ManufacturingOrderDTO): NgbModalRef {
         const modalRef = this.modalService.open(component, { size: 'lg', backdrop: 'static'});
+        const manufacturingOrder: ManufacturingOrder = manufacturingOrderDTO.manufacturingOrder;
+        manufacturingOrder.orderDate = this.datePipe.transform(manufacturingOrder.orderDate, 'yyyy-MM-ddTHH:mm:ss');
         modalRef.componentInstance.manufacturingOrder = manufacturingOrder;
+        modalRef.componentInstance.moProducts = manufacturingOrderDTO['mOProducts'];
+        modalRef.componentInstance.products = manufacturingOrderDTO.products;
+        modalRef.componentInstance.stAttributeValues = manufacturingOrderDTO.sTAttributeValues;
+
+        modalRef.componentInstance.supplies = [];
+        modalRef.componentInstance.attributeValues = [];
+
+        let productPosition = -1;
+        let supplyPosition = -1;
+        let productId = -1;
+        let supplyId = -1;
+        for (let stAttributeValuePosition = 0; stAttributeValuePosition < modalRef.componentInstance.stAttributeValues.length; stAttributeValuePosition++) {
+            const stAttributeValue: STAttributeValue = modalRef.componentInstance.stAttributeValues[stAttributeValuePosition];
+            const stAttribute: STAttribute = stAttributeValue.stAttribute;
+            const supplyType: SupplyType = stAttributeValue.supplyType;
+            const supply: Supply = stAttributeValue.supply;
+            const product: Product = stAttributeValue.product;
+
+            if (productId !== product.id) {
+                productPosition++; productId = product.id;
+            }
+            if (supplyId !== supply.id) {
+                supplyPosition++; supplyId = supply.id;
+            }
+
+            if (!modalRef.componentInstance.attributeValues[productPosition]) {
+                modalRef.componentInstance.attributeValues[productPosition] = [];
+            }
+            if (!modalRef.componentInstance.attributeValues[productPosition][supplyPosition]) {
+                modalRef.componentInstance.attributeValues[productPosition][supplyPosition] = {};
+            }
+
+            modalRef.componentInstance.attributeValues[productPosition][supplyPosition][stAttribute.name] = stAttributeValue.value;
+            if (!modalRef.componentInstance.supplies[productPosition + supplyPosition]) {
+                modalRef.componentInstance.supplies[productPosition + supplyPosition] = supply;
+                modalRef.componentInstance.supplies[productPosition + supplyPosition].supplyType.stattributes = [];
+            }
+            modalRef.componentInstance.supplies[productPosition + supplyPosition].supplyType.stattributes.push(stAttribute);            
+            if (!modalRef.componentInstance.products[productPosition].supplies) {
+                modalRef.componentInstance.products[productPosition].supplies = [];
+            }
+            modalRef.componentInstance.products[productPosition].supplies.push(supply);
+        }
+
         modalRef.result.then((result) => {
             this.router.navigate([{ outlets: { popup: null }}], { replaceUrl: true, queryParamsHandling: 'merge' });
             this.ngbModalRef = null;
